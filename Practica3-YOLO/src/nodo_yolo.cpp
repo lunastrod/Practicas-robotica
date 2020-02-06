b1787733ba9205056058171426a570575c04507b
DanParSeg <daniel.parra.segovia@gmail.com>
2020-02-06 18:30:15 +0100

    estructura vacía

---
+++ Practica3-YOLO/src/nodo_yolo.cpp
@@ -0,0 +1,19 @@
+#include "ros/ros.h"
+
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Vector3.h"
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "bumpgo");
+  ros::Rate loop_rate(20);
+  while (ros::ok())
+  {
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
