3ae01f8fcd332ceebb5490431e4e5503b0d15e66
DanParSeg <daniel.parra.segovia@gmail.com>
2020-02-06 18:40:55 +0100

    funciona el launcher

--- Practica3-YOLO/src/nodo_yolo.cpp
+++ Practica3-YOLO/src/nodo_yolo.cpp
@@ -4,9 +4,11 @@
 #include "geometry_msgs/Vector3.h"
 
 
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "bumpgo");
+  ros::NodeHandle n_;
   ros::Rate loop_rate(20);
   while (ros::ok())
   {
