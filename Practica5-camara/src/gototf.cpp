59a292a3a623ca28b815df7fb84d08bbac665cdd
astrod <daniel.parra.segovia@gmail.com>
2020-02-18 19:28:46 +0100

    calcula distancia y angulo

--- Practica5-camara/src/gototf.cpp
+++ Practica5-camara/src/gototf.cpp
@@ -2,20 +2,31 @@
 
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Vector3.h"
-#include "sensor_msgs/LaserScan.h"
-#include "sensor_msgs/CameraInfo.h"
+#include <tf/transform_listener.h>
+
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "gototf");
   ros::NodeHandle n_;
+  tf::TransformListener tfListener_;
 
-  
 
   ros::Rate loop_rate(20);
   while (ros::ok())
   {
-    printf("hola\n");
+    tf::StampedTransform transform;
+    try{
+      tfListener_.lookupTransform("/base_footprint", "/object/0", ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+      ROS_ERROR("%s",ex.what());
+    }
+    printf("ang=%f\n", atan2(transform.getOrigin().y(),transform.getOrigin().x()));
+    printf("dist=%f\n",sqrt(pow(transform.getOrigin().x(), 2) + pow(transform.getOrigin().y(), 2)));
+
+
     ros::spinOnce();
     loop_rate.sleep();
   }
