9a4b1b4f37cd8f868ff1e0a041bf23a59bd4d547
astrod <daniel.parra.segovia@gmail.com>
2020-02-21 21:18:03 +0100

    busquedap6 compila

--- Practica6-botella/src/busquedap6.cpp
+++ Practica6-botella/src/busquedap6.cpp
@@ -1,31 +1,37 @@
-#include "ros/ros.h"
+#include "stdio.h"
 
+#include "ros/ros.h"
 //#include <tf/transform_listener.h>
 //#include "actionlib/client/simple_action_server.h"
 #include "sensor_msgs/CameraInfo.h"
 #include "darknet_ros_msgs/BoundingBoxes.h"
 
-class camera{
+class camara{
 public:
-  camera(){
+  camara(){
     ros::NodeHandle n_;
     sub_objetos_ = n_.subscribe("/darknet_ros/bounding_boxes", 1, &camara::boxesCallBack, this);
     sub_camera_ = n_.subscribe("/camera/rgb/camera_info", 1, &camara::cameraCallBack, this);
-
   }
   void cameraCallBack(const sensor_msgs::CameraInfo& msg){
     image_width = msg.width;
+    image_height = msg.height;
   }
   void boxesCallBack(const darknet_ros_msgs::BoundingBoxes& msg){
     objeto_detectado_ = false;
-    if(!msg.bounding_boxes[0].Class.compare("person")){
+    if(!msg.bounding_boxes[0].Class.compare(tipo_objeto)){
       centrox = (msg.bounding_boxes[0].xmin + msg.bounding_boxes[0].xmax) / 2;
+      centroy = (msg.bounding_boxes[0].ymin + msg.bounding_boxes[0].ymax) / 2;
       objeto_detectado_ = true;
+      ROS_INFO("objeto detectado en (%d,%d)\n",centrox,centroy);
     }
   }
 private:
+  const std::string tipo_objeto = "person";
   int image_width = 640;
+  int image_height = 480;
   int centrox;
+  int centroy;
   bool objeto_detectado_ = false;
   ros::Subscriber sub_objetos_;
   ros::Subscriber sub_camera_;
@@ -34,7 +40,7 @@ private:
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "busquedap6");
-  camera c;
+  camara cam;
   ros::Rate loop_rate(20);
   while (ros::ok())
   {
