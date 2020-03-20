fe8f1233a971d7a23e03882cc48870f31778be91
juanlroper <juanlroper@gmail.com>
2020-03-20 17:52:29 +0100

    main

--- Practica6-botella/src/mainp6.cpp
+++ Practica6-botella/src/mainp6.cpp
@@ -3,13 +3,19 @@
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Vector3.h"
 #include <tf/transform_listener.h>
+#include "softarq_msgs/Distance.h"
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "navp6");
   ros::Rate loop_rate(20);
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<softarq_msgs::Distance>("detecta_obj");
+  softarq_msgs::Distance srv;
+
   while (ros::ok())
   {
+    client.call(srv);
     printf("Main\n");
     ros::spinOnce();
     loop_rate.sleep();
