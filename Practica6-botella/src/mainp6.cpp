1067203207fac13cd53e89c2e46ce3828aa1697b
astrod <daniel.parra.segovia@gmail.com>
2020-03-20 17:46:31 +0100

    main

---
+++ Practica6-botella/src/mainp6.cpp
@@ -0,0 +1,18 @@
+#include "ros/ros.h"
+
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Vector3.h"
+#include <tf/transform_listener.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "navp6");
+  ros::Rate loop_rate(20);
+  while (ros::ok())
+  {
+    printf("Main\n");
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
