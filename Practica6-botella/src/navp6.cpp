ea021635ce400659e735355fcf876f4e2945c65e
astrod <daniel.parra.segovia@gmail.com>
2020-03-23 10:37:32 +0100

    navp6

--- Practica6-botella/src/navp6.cpp
+++ Practica6-botella/src/navp6.cpp
@@ -10,16 +10,36 @@
 class Navigator
 {
   public:
-    Navigator(ros::NodeHandle& nh) : nh_(nh), action_client_("/move_base", false), goal_sended_(false)
+    Navigator(ros::NodeHandle& nh) : nh_(nh), action_client_("/move_base", false), goal_sent_(false)
     {
-      POSITION[0].x=1;
+      navigator_goals_sub = n_.subscribe("navigator_goals", 1, &Navigator::goalscallback, this);
+      POSITION[0].x=0;
       POSITION[0].y=0;
       POSITION[0].z=0;
+
+      POSITION[1].x=10;
+      POSITION[1].y=0;
+      POSITION[1].z=0;
+
+      POSITION[2].x=0;
+      POSITION[2].y=10;
+      POSITION[2].z=0;
+
+      POSITION[3].x=10;
+      POSITION[3].y=10;
+      POSITION[3].z=0;
+    }
+
+    void goalscallback(const int& msg){
+      if(msg<NUMBER_GOALS){
+        current_goal=msg;
+      }
     }
 
     void step()
     {
-      if (goal_sended_)
+      gotogoal(current_goal);
+      if (goal_sent_)
       {
         bool finished_before_timeout = action_client_.waitForResult(ros::Duration(0.5));
         actionlib::SimpleClientGoalState state = action_client_.getState();
@@ -30,37 +50,43 @@ class Navigator
             ROS_INFO("[navigate_to_wp] Goal Reached!");
           else
             ROS_INFO("[navigate_to_wp] Something bad happened!");
-          goal_sended_ = false;
+          goal_sent_ = false;
         }
       }
     }
 
     void gotogoal(int n)
     {
-      geometry_msgs::PoseStamped goal_pose_;
-      goal_pose_.pose.position=POSITION[0];
-      goal_pose_.pose.orientation.x=0;
-      goal_pose_.pose.orientation.y=0;
-      goal_pose_.pose.orientation.z=0;
-      goal_pose_.pose.orientation.w=1;
+      if(!goal_sent_){
+        geometry_msgs::PoseStamped goal_pose_;
+        goal_pose_.pose.position=POSITION[0];
+        goal_pose_.pose.orientation.x=0;
+        goal_pose_.pose.orientation.y=0;
+        goal_pose_.pose.orientation.z=0;
+        goal_pose_.pose.orientation.w=1;
 
-      ROS_INFO("[navigate_to_wp] Commanding to (%f %f)", goal_pose_.pose.position.x, goal_pose_.pose.position.y);
+        ROS_INFO("[navigate_to_wp] Commanding to (%f %f)", goal_pose_.pose.position.x, goal_pose_.pose.position.y);
 
-      move_base_msgs::MoveBaseGoal goal;
-      goal.target_pose = goal_pose_;
-      goal.target_pose.header.frame_id = "map";
-      goal.target_pose.header.stamp = ros::Time::now();
-      action_client_.sendGoal(goal);
-      goal_sended_ = true;
+        move_base_msgs::MoveBaseGoal goal;
+        goal.target_pose = goal_pose_;
+        goal.target_pose.header.frame_id = "map";
+        goal.target_pose.header.stamp = ros::Time::now();
+        action_client_.sendGoal(goal);
+        goal_sent_ = true;
+      }
     }
 
   private:
     ros::NodeHandle nh_;
     ros::Subscriber wp_sub_;
-    bool goal_sended_;
+    ros::Subscriber navigator_goals_sub;
     actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> action_client_;
 
-    geometry_msgs::Point POSITION[4];
+    int NUMBER_GOALS=4;
+    bool goal_sent_;
+    int current_goal;
+
+    geometry_msgs::Point POSITION[NUMBER_GOALS];
 
 
 };
@@ -73,7 +99,6 @@ int main(int argc, char **argv)
   ros::Rate loop_rate(1);
   while (ros::ok())
   {
-    navigator.gotogoal(0);
     navigator.step();
     ros::spinOnce();
     loop_rate.sleep();
